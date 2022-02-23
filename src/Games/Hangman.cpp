a34f149c0dfed26ab6e36fbfa14d31346c5b0dd7
Emma <emma.pernin1@gmail.com>
2022-02-23 17:59:00 +0100

    [Hangman] Add get_character_from_player, show_number_of_lives and player_is_alive

--- src/Games/Hangman.cpp
+++ src/Games/Hangman.cpp
@@ -17,9 +17,33 @@ static std::string pick_a_word_to_guess()
     return words[element_number];
 }
 
+static char get_character_from_player()
+{
+    char choosen_character = '\0';
+    std::cout << "Pick a letter : ";
+    std::cin >> choosen_character;
+    return static_cast<char>(tolower(choosen_character));
+}
+
+static void show_number_of_lives(int number_of_lives)
+{
+    std::cout << "You've got " << std::to_string(number_of_lives) << " lives." << std::endl;
+}
+
+static bool player_is_alive(int number_of_lives)
+{
+    return (number_of_lives > 0);
+}
+
+static bool player_has_won()
+{
+    return true;
+}
+
 void play_hangman()
 {
     for (size_t i = 0; i < 10; i++) {
-        std::cout << pick_a_word_to_guess() << std::endl;
+        // std::cout << pick_a_word_to_guess() << std::endl;
+        std::cout << get_character_from_player() << std::endl;
     }
 }
