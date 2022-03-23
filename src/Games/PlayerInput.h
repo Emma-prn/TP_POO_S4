728f8124526011126d8d4201c1192a8e7a9bbaba
Emma <emma.pernin1@gmail.com>
2022-03-23 14:22:50 +0100

    [PlayerInput] Update get_input_from_the_player

--- src/Games/PlayerInput.h
+++ src/Games/PlayerInput.h
@@ -8,17 +8,6 @@ T get_input_from_the_player()
     while (!(std::cin >> input)) {
         std::cin.clear();
         std::cout << "Invalide input" << std::endl;
-    }
-    return static_cast<char>(tolower(input));
-}
-
-template<>
-int get_input_from_the_player<int>()
-{
-    int input;
-    while ((std::cout << "Please choose a number between 0 and 100 : ") && (!(std::cin >> input) || input < 0 || input > 100)) {
-        std::cout << "That's not a number between 0 and 100. ";
-        std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
     return input;
