c9d5eb8f9b2e5f6324b79f4fae897a047ec1a910
Emma <emma.pernin1@gmail.com>
2022-03-23 14:53:01 +0100

    [PlayerInput] Update get_input_from_the_player

--- src/Games/PlayerInput.h
+++ src/Games/PlayerInput.h
@@ -6,8 +6,8 @@ T get_input_from_the_player()
 {
     T input;
     while (!(std::cin >> input)) {
-        std::cin.clear();
         std::cout << "Invalide input" << std::endl;
+        std::cin.clear();
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
     return input;
