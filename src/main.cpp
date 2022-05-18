4d6862b8b00b542e65833fc93bc16eba2bcb4bba
Emma <emma.pernin1@gmail.com>
2022-05-18 14:46:14 +0200

    [main] Put back the menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
 #include "Games/Menu.h"
-#include "Games/NoughtAndCross.h"
 
 int main()
 {
-    // show_menu();
-    play_nought_and_cross();
+    show_menu();
 }
